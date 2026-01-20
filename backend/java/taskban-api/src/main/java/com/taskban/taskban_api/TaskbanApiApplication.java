d73b23b7cf282bfca9110a617eede43d19f96018
Alessandra <alessandra.rcode@gmail.com>
2026-01-20 20:45:28 -0300

    Adicionando arquivo Spring - (taskban-api).

---
+++ backend/java/taskban-api/src/main/java/com/taskban/taskban_api/TaskbanApiApplication.java
@@ -0,0 +1,13 @@
+package com.taskban.taskban_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskbanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskbanApiApplication.class, args);
+	}
+
+}
---
+++ backend/java/taskban-api/src/test/java/com/taskban/taskban_api/TaskbanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskban.taskban_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskbanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
